c2ce9ae18c39baef7d68e19528dbf1139b2b0cb8
fekt2016 <fekt2016@icloud.com>
2025-10-02 20:16:56 +0000

    controllers

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,7 @@
 exports.signup = (req, res) => {
   const { fullName, phone, password, passwordConfirm, email } = req.body;
   console.log(fullName, phone, password, passwordConfirm, email);
+  //good
   res.send("signup");
 };
 exports.login = (req, res) => {
