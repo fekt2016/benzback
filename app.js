f757cf80ccbcd4c2f410728c56189d0bcdf02652
fekt2016 <fekt2016@icloud.com>
2025-10-02 22:48:58 +0000

    issues with routes

--- app.js
+++ app.js
@@ -58,6 +58,14 @@ app.use((req, res, next) => {
 app.use("/api/v1/auth", authRouter);
 // app.use("/api/users", userRouter);
 
+console.log("🚀 Registered routes:");
+console.log(
+  app._router.stack
+    .filter((r) => r.route) // only keep layers with a route
+    .map(
+      (r) => `${Object.keys(r.route.methods)[0].toUpperCase()} ${r.route.path}`
+    )
+);
 // Global error handler
 app.use((err, req, res, next) => {
   console.log(req.config);
@@ -67,13 +75,5 @@ app.use((err, req, res, next) => {
     message: err.message || "Internal Server Error",
   });
 });
-console.log("🚀 Registered routes:");
-console.log(
-  app._router.stack
-    .filter((r) => r.route) // only keep layers with a route
-    .map(
-      (r) => `${Object.keys(r.route.methods)[0].toUpperCase()} ${r.route.path}`
-    )
-);
 
 module.exports = app;
