565ef68e993c528c5ca108ef3ea5410ef9362ae5
fekt2016 <fekt2016@icloud.com>
2025-10-02 20:11:58 +0000

    setting up routes

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,6 @@
 exports.signup = (req, res) => {
+  const { fullName, phone, password, passwordConfirm, email } = req.body;
+  console.log(fullName, phone, password, passwordConfirm, email);
   res.send("signup");
 };
 exports.login = (req, res) => {
